c64d617496ea33a2d96eb1aa9ded4a39fc906c9e
Anthony Famosa <anthonyf.cad@gmail.com>
2021-01-30 18:40:35 +0000

    axios.get()
    making a get request from the site

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
+import axios from 'axios';
 
 export default class Discover extends Component {
   constructor() {
@@ -12,6 +13,31 @@ export default class Discover extends Component {
       categories: []
     };
   }
+   async componentDidMount() {
+     await axios.get(`https://api.spotify.com/v1/browse/new-releases`)
+       .then(res => {
+         const newReases = res.data;
+         this.setState({ newReleases });
+       })
+
+     await axios.get(`https://api.spotify.com/v1/me/playlists`)
+       .then(res => {
+         const playlists = res.data;
+         this.setState({ playlists });
+       })
+       await axios.get(`https://api.spotify.com/v1/browse/categories/{category_id}`)
+       .then(res => {
+         const categories = res.data;
+         this.setState({ categories });
+      })
+      }
+
+// GET https://api.spotify.com/v1/browse/new-releases
+// GET https://api.spotify.com/v1/me/playlists
+// GET https://api.spotify.com/v1/browse/categories/{category_id}
+  
+
+
 
   render() {
     const { newReleases, playlists, categories } = this.state;
