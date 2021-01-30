8018dc80b97ba1d2572a56acec17112bad7d3023
Anthony Famosa <anthonyf.cad@gmail.com>
2021-01-30 16:56:25 +0000

    Update config.js

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: '53Y8wTMz5H4WQ8O2c',
+    clientSecret: 'MY_CLIENT_SECRET_SAMPLE'
   }
 }
